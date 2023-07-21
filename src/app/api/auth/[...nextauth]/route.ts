62236af0867057ece66f9b5cbb6079346b8c08c4
DanielZ1337 <danielz2nd@hotmail.com>
2023-07-22 00:45:56 +0200

    nextauth edge?

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import {authOptions} from "@/lib/auth";
 import NextAuth from "next-auth";
 
+export const runtime = 'edge'
+
 const handler = NextAuth(authOptions);
 export {handler as GET, handler as POST};
