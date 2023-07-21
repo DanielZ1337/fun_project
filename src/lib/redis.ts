35b4611851807ed05d701279cbd92e65a9d65aea
DanielZ1337 <danielz2nd@hotmail.com>
2023-07-22 00:05:33 +0200

    redis fix

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,3 @@
 import {Redis} from '@upstash/redis'
 
-export const redisClient = new Redis({
-    url: 'https://topical-molly-45045.upstash.io',
-    token: 'Aa_1ASQgYzY5MzFiNTEtZGU1OC00ODUyLWEyNTUtZDM3OTMwOTFjY2I4NzdkNzQ1MjE4YjA4NDZjODkxMzlhZGZmMjNhYTBjNDE=',
-})
+export const redisClient = Redis.fromEnv()
