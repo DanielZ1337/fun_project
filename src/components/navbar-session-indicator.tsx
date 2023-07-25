cb73cfc01f4df7013277fec0a59b0af0515be36b
DanielZ1337 <danielz2nd@hotmail.com>
2023-07-25 22:25:34 +0200

    fixed some minor things with logging in status loading

--- src/components/navbar-session-indicator.tsx
+++ src/components/navbar-session-indicator.tsx
@@ -2,15 +2,44 @@ import {signIn, useSession} from "next-auth/react";
 import {Spinner} from "@/components/icons";
 import {AccountDropdown} from "@/components/account-dropdown";
 import Button from "@/components/ui/button";
+import React from "react";
+import {useToast} from "@/hooks/useToast";
 
 export default function NavbarSessionIndicator() {
     const {data: session, status} = useSession();
+    const [isLoggingIn, setIsLoggingIn] = React.useState(false)
+    const useToaster = useToast()
+
+    async function handleLogin() {
+        try {
+            setIsLoggingIn(true)
+            await signIn('github')
+        } catch (e) {
+            setIsLoggingIn(false)
+            useToaster.toast({
+                title: "Error",
+                description: "An error occurred while logging in.",
+            })
+        }
+
+    }
 
     if (status === "loading") return <Spinner/>;
     if (status === "authenticated") return <AccountDropdown session={session}/>;
-    if (status === "unauthenticated") return <Button className={"rounded-md p-2"} onClick={() => signIn('github')}>
-        Login
-    </Button>;
+    if (status === "unauthenticated") {
+        if (isLoggingIn) {
+            return (
+                <div className={"flex gap-2 items-center"}>
+                    <Spinner className={"w-5 h-5"}/>
+                    <p className={"text-sm font-medium text-gray-500"}>Logging in...</p>
+                </div>
+            )
+        } else {
+            return <Button className={"rounded-md p-2"} disabled={isLoggingIn} onClick={handleLogin}>
+                Login
+            </Button>
+        }
+    }
 
     return null;
 }
