48e6da932cf5eba9bda666c2611732287bf154f6
DanielZ1337 <danielz2nd@hotmail.com>
2023-07-25 07:37:22 +0200

    clean up

--- src/components/backlink-navigation-menu-preview.tsx
+++ src/components/backlink-navigation-menu-preview.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
 
 interface Props {
+    owner: string
+    repo: string
     title: string
     excerpt: string
     slug: string
 }
 
-export default function BacklinkNavigationMenuPreview({title, excerpt, slug}: Props) {
+export default function BacklinkNavigationMenuPreview({owner, repo, title, excerpt, slug}: Props) {
     return (
-        <Link href={`/notes/${slug}`}
+        <Link href={`/notes/${owner}/${repo}/${slug}`}
               className='block p-4 border border-gray-200 rounded-md hover:bg-neutral-100 dark:hover:bg-neutral-900'>
             <h3 className='text-lg font-semibold'>{title}</h3>
             <p className='mt-1 text-sm text-gray-500'>{excerpt}</p>
