7069a0e2b00151085af238a44b55dc1feba8d38c
DanielZ1337 <danielz2nd@hotmail.com>
2023-07-26 09:08:24 +0200

    minor fixes

--- src/components/post-header.tsx
+++ src/components/post-header.tsx
@@ -6,15 +6,13 @@ export default function PostHeader({post}: { post: Post }) {
     return (
         <div className="mx-auto py-4 px-6 border-b border-gray-300 w-full flex flex-col gap-4">
             <h1 className="text-3xl font-bold">{post.metadata.title}</h1>
-            <div>
-                <Link href={`https://github.com/${post.metadata.author?.name}`} className="flex items-center">
-                    <Avatar className={"w-12 h-12 rounded-full mr-4"}>
-                        <AvatarImage src={post.metadata.author?.picture} alt={post.metadata.author?.name}/>
-                        <AvatarFallback>{post.metadata.author?.name.charAt(0).toUpperCase()}</AvatarFallback>
-                    </Avatar>
-                    <span className="font-bold text-lg">{post.metadata.author?.name}</span>
-                </Link>
-            </div>
+            <Link href={`https://github.com/${post.metadata.author?.name}`} className="flex items-center w-fit">
+                <Avatar className={"w-12 h-12 rounded-full mr-4"}>
+                    <AvatarImage src={post.metadata.author?.picture} alt={post.metadata.author?.name}/>
+                    <AvatarFallback>{post.metadata.author?.name.charAt(0).toUpperCase()}</AvatarFallback>
+                </Avatar>
+                <span className="font-bold text-lg">{post.metadata.author?.name}</span>
+            </Link>
             <div>
                 <p className="text-gray-600">Created: {post.metadata.date}</p>
                 <p className="text-gray-600">Updated: {post.metadata.updatedAt}</p>
